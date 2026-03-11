371a37cebfb4dff46853da1b80f2a9c101b4b8bf
aej12 <148771487+aej12@users.noreply.github.com>
2026-03-11 22:38:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const btn=document.getElementById("calcBtn")
 
-let chart
+let chart1
+let chart2
 
-function format(num){
-return Math.round(num).toLocaleString()
+function format(n){
+
+return Math.round(n).toLocaleString()
+
 }
 
 btn.onclick=function(){
@@ -11,7 +14,7 @@ btn.onclick=function(){
 let age=+ageSelf.value
 let ageP=+agePartner.value
 
-let assetValue=+asset.value*10000
+let asset=+asset.value*10000
 let r=rate.value/100
 
 let inc1=incomeSelf.value*10000
@@ -29,87 +32,107 @@ let childN=childCount.value
 let childIndep=childIndep.value
 let childCost=childCost.value*10000
 
+
 let labels=[]
 let assets=[]
 let fireLine=[]
 
-let tableBody=document.querySelector("#resultTable tbody")
-tableBody.innerHTML=""
+let incomeArr=[]
+let expenseArr=[]
+let childArr=[]
 
 let fireAge=null
 let bankruptAge=null
 
-let fireTargetNow=expenseBase*25
 
-fireTarget.innerText=format(fireTargetNow/10000)+"만원"
+let tableBody=document.querySelector("#resultTable tbody")
+
+tableBody.innerHTML=""
+
 
 for(let i=0;i<80;i++){
 
 let ageNow=age+i
-let agePNow=ageP+i
 
 let income=0
 
 if(ageNow<retire1) income+=inc1*Math.pow(1+g,i)
-if(agePNow<retire2) income+=inc2*Math.pow(1+g,i)
+if(ageP+i<retire2) income+=inc2*Math.pow(1+g,i)
 
 let expense=expenseBase*Math.pow(1+infl,i)
 
+let childExpense=0
+
 if(ageNow<childIndep){
-expense+=childCost*childN*Math.pow(1+infl,i)
+
+childExpense=childCost*childN*Math.pow(1+infl,i)
+
 }
+
+expense+=childExpense
+
+
+asset=asset*(1+r)+income-expense
 
 let fireTarget=expense*25
 
-assetValue=assetValue*(1+r)+income-expense
 
-if(assetValue<=0 && !bankruptAge){
-bankruptAge=ageNow
-}
+if(asset<=0 && !bankruptAge) bankruptAge=ageNow
 
-let passive=assetValue*r
+if(asset*r>=expense && !fireAge) fireAge=ageNow
 
-if(!fireAge && passive>=expense){
-fireAge=ageNow
-}
 
-labels.push(ageNow+" / "+agePNow)
-assets.push(assetValue)
+labels.push(ageNow)
+
+assets.push(asset)
+
 fireLine.push(fireTarget)
 
-let row=`
+incomeArr.push(income)
+
+expenseArr.push(expense)
+
+childArr.push(childExpense)
+
+
+tableBody.innerHTML+=`
+
 <tr>
-<td>${ageNow}</td>
-<td>${agePNow}</td>
-<td>${format(assetValue)}</td>
+<td>${ageNow}/${ageP+i}</td>
+<td>${format(asset)}</td>
 <td>${format(income)}</td>
 <td>${format(expense)}</td>
 </tr>
+
 `
 
-tableBody.innerHTML+=row
-
 }
+
 
 resultBox.style.display="block"
 
+
 if(bankruptAge){
 
-resultText.innerHTML=`이대로라면 <b>${bankruptAge}세</b>에 자산이 고갈됩니다.`
-
-}else if(fireAge){
-
-resultText.innerHTML=`이대로라면 <b>${fireAge}세</b>에 경제적 자유에 도달합니다.`
+mainResult.innerHTML=`${bankruptAge}세에 자산이 고갈됩니다.`
 
 }else{
 
-resultText.innerHTML=`100세까지 경제적 자유에 도달하지 못합니다.`
+mainResult.innerHTML=`${fireAge}세에 경제적 자유(FIRE)에 도달합니다.`
 
 }
 
-if(chart) chart.destroy()
 
-chart=new Chart(assetChart,{
+assumption1.innerText=`현재 기준 월 ${(inc1+inc2)/12/10000}만원 수입 / 월 ${expenseBase/12/10000}만원 지출`
+
+assumption2.innerText=`소득 증가율 ${incomeGrowth.value}% / 지출 증가율 ${inflation.value}%`
+
+assumption3.innerText=`투자수익률 ${rate.value}% 가정`
+
+
+if(chart1) chart1.destroy()
+
+chart1=new Chart(assetChart,{
 
 type:"line",
 
@@ -118,14 +141,38 @@ labels:labels,
 datasets:[
 {
 label:"순자산",
-data:assets,
-borderWidth:3
+data:assets
 },
 {
 label:"FIRE 목표자산",
-data:fireLine,
-borderDash:[6,6],
-borderWidth:3
+data:fireLine
+}
+]
+}
+
+})
+
+
+if(chart2) chart2.destroy()
+
+chart2=new Chart(flowChart,{
+
+type:"line",
+
+data:{
+labels:labels,
+datasets:[
+{
+label:"수입",
+data:incomeArr
+},
+{
+label:"지출",
+data:expenseArr
+},
+{
+label:"자녀지출",
+data:childArr
 }
 ]
 }
